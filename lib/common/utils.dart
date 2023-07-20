9dd86c36ae61c9e768e62717268839af27f725b4
Risman Abdilah <risman.abdilah@frisidea.com>
2023-07-20 16:59:34 +0700

    Refactor Clean Architecture for  TVSeries Feature

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -1,3 +1,43 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../features/movie/presentation/pages/about_page.dart';
+import '../features/movie/presentation/pages/home_movie_page.dart';
+import '../features/movie/presentation/pages/movie_detail_page.dart';
+import '../features/movie/presentation/pages/popular_movies_page.dart';
+import '../features/movie/presentation/pages/search_page.dart';
+import '../features/movie/presentation/pages/top_rated_movies_page.dart';
+import '../features/movie/presentation/pages/watchlist_movies_page.dart';
 
 final RouteObserver<ModalRoute> routeObserver = RouteObserver<ModalRoute>();
+
+Route onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case HomeMoviePage.ROUTE_NAME:
+      return MaterialPageRoute(builder: (_) => HomeMoviePage());
+    case PopularMoviesPage.ROUTE_NAME:
+      return CupertinoPageRoute(builder: (_) => PopularMoviesPage());
+    case TopRatedMoviesPage.ROUTE_NAME:
+      return CupertinoPageRoute(builder: (_) => TopRatedMoviesPage());
+    case MovieDetailPage.ROUTE_NAME:
+      final id = settings.arguments as int;
+      return MaterialPageRoute(
+        builder: (_) => MovieDetailPage(id: id),
+        settings: settings,
+      );
+    case SearchPage.ROUTE_NAME:
+      return CupertinoPageRoute(builder: (_) => SearchPage());
+    case WatchlistMoviesPage.ROUTE_NAME:
+      return MaterialPageRoute(builder: (_) => WatchlistMoviesPage());
+    case AboutPage.ROUTE_NAME:
+      return MaterialPageRoute(builder: (_) => AboutPage());
+    default:
+      return MaterialPageRoute(builder: (_) {
+        return Scaffold(
+          body: Center(
+            child: Text('Page not found :('),
+          ),
+        );
+      });
+  }
+}
--- lib/injection.dart
+++ lib/injection.dart
@@ -1,26 +1,27 @@
-import 'package:ditonton/data/datasources/db/database_helper.dart';
-import 'package:ditonton/data/datasources/movie_local_data_source.dart';
-import 'package:ditonton/data/datasources/movie_remote_data_source.dart';
-import 'package:ditonton/data/repositories/movie_repository_impl.dart';
-import 'package:ditonton/domain/repositories/movie_repository.dart';
-import 'package:ditonton/domain/usecases/get_movie_detail.dart';
-import 'package:ditonton/domain/usecases/get_movie_recommendations.dart';
-import 'package:ditonton/domain/usecases/get_now_playing_movies.dart';
-import 'package:ditonton/domain/usecases/get_popular_movies.dart';
-import 'package:ditonton/domain/usecases/get_top_rated_movies.dart';
-import 'package:ditonton/domain/usecases/get_watchlist_movies.dart';
-import 'package:ditonton/domain/usecases/get_watchlist_status.dart';
-import 'package:ditonton/domain/usecases/remove_watchlist.dart';
-import 'package:ditonton/domain/usecases/save_watchlist.dart';
-import 'package:ditonton/domain/usecases/search_movies.dart';
-import 'package:ditonton/presentation/provider/movie_detail_notifier.dart';
-import 'package:ditonton/presentation/provider/movie_list_notifier.dart';
-import 'package:ditonton/presentation/provider/movie_search_notifier.dart';
-import 'package:ditonton/presentation/provider/popular_movies_notifier.dart';
-import 'package:ditonton/presentation/provider/top_rated_movies_notifier.dart';
-import 'package:ditonton/presentation/provider/watchlist_movie_notifier.dart';
 import 'package:http/http.dart' as http;
 import 'package:get_it/get_it.dart';
+
+import 'features/movie/data/datasources/db/database_helper.dart';
+import 'features/movie/data/datasources/movie_local_data_source.dart';
+import 'features/movie/data/datasources/movie_remote_data_source.dart';
+import 'features/movie/data/repositories/movie_repository_impl.dart';
+import 'features/movie/domain/repositories/movie_repository.dart';
+import 'features/movie/domain/usecases/get_movie_detail.dart';
+import 'features/movie/domain/usecases/get_movie_recommendations.dart';
+import 'features/movie/domain/usecases/get_now_playing_movies.dart';
+import 'features/movie/domain/usecases/get_popular_movies.dart';
+import 'features/movie/domain/usecases/get_top_rated_movies.dart';
+import 'features/movie/domain/usecases/get_watchlist_movies.dart';
+import 'features/movie/domain/usecases/get_watchlist_status.dart';
+import 'features/movie/domain/usecases/remove_watchlist.dart';
+import 'features/movie/domain/usecases/save_watchlist.dart';
+import 'features/movie/domain/usecases/search_movies.dart';
+import 'features/movie/presentation/provider/movie_detail_notifier.dart';
+import 'features/movie/presentation/provider/movie_list_notifier.dart';
+import 'features/movie/presentation/provider/movie_search_notifier.dart';
+import 'features/movie/presentation/provider/popular_movies_notifier.dart';
+import 'features/movie/presentation/provider/top_rated_movies_notifier.dart';
+import 'features/movie/presentation/provider/watchlist_movie_notifier.dart';
 
 final locator = GetIt.instance;
 
